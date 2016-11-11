a619b16a3d85ce0ef8d308db13acb3e8a2f3e72b
nathan <nathan@vertile.com>
2016-11-10 22:18:05 -0800

    ms5611 baro

--- src/main/target/OMNIBUSF4/target.mk
+++ src/main/target/OMNIBUSF4/target.mk
@@ -3,6 +3,7 @@ FEATURES       += VCP ONBOARDFLASH SDCARD
 
 TARGET_SRC = \
             drivers/accgyro_spi_mpu6000.c \
+            drivers/barometer_ms5611.c \
             drivers/barometer_bmp280.c \
             drivers/barometer_spi_bmp280.c \
             drivers/compass_hmc5883l.c \
